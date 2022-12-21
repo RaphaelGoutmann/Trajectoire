e13ab8834403eb7aa940f0ff36cbff4bfe538787
Raphael Goutmann <raphaelgoutmann@gmail.com>
2022-12-21 18:04:18 +0100

    Modification des liens static et media dans settings.py

--- src/trajectoire/settings.py
+++ src/trajectoire/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 
 # Default primary key field type
@@ -120,5 +120,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "media/"
 
